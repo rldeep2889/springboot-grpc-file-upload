dea3b70d014b128ee7c6897d69a0db5ff4bf0c92
alphadeepak <95413347+alphadeepak@users.noreply.github.com>
2022-01-04 20:44:46 +0530

    Initial commit with proto file

---
+++ springboot-grpc-file-upload/src/main/java/com/grpc/file/upload/GrpcFileUploadApplication.java
@@ -0,0 +1,12 @@
+package com.grpc.file.upload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrpcFileUploadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrpcFileUploadApplication.class, args);
+    }
+}
